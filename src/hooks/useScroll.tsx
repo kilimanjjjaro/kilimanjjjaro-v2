af39e6dc3f109cd2fe443de1a895fb410c377a80
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-06-24 02:49:38 -0300

    Improve hook

--- src/hooks/useScroll.tsx
+++ src/hooks/useScroll.tsx
@@ -1,27 +1,30 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 export default function useScroll() {
   const [isVisible, setIsVisible] = useState(true)
   const [scrollPercentage, setScrollPercentage] = useState(0)
+  const prevScrollPosRef = useRef(0)
+
+  const calculateScrollPercentage = useCallback(() => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+    const currentScrollPercentage = Math.round(
+      (scrollTop / (scrollHeight - clientHeight)) * 100
+    )
+    setScrollPercentage(currentScrollPercentage)
+  }, [])
+
+  const handleScroll = useCallback(() => {
+    const currentScrollPos = window.scrollY
+    const isScrollingUp = prevScrollPosRef.current > currentScrollPos
+
+    setIsVisible(isScrollingUp)
+    prevScrollPosRef.current = currentScrollPos
+
+    calculateScrollPercentage()
+  }, [])
 
   useEffect(() => {
-    let prevScrollPos = window.scrollY
-
-    const handleScroll = () => {
-      const currentScrollPos = window.scrollY
-      const isScrollingUp = prevScrollPos > currentScrollPos
-
-      setIsVisible(isScrollingUp)
-
-      prevScrollPos = currentScrollPos
-
-      const { scrollTop, scrollHeight, clientHeight } = document.documentElement
-      const currentScrollPercentage = Math.round(
-        (scrollTop / (scrollHeight - clientHeight)) * 100
-      )
-
-      setScrollPercentage(currentScrollPercentage)
-    }
+    calculateScrollPercentage()
 
     window.addEventListener('scroll', handleScroll)
 
