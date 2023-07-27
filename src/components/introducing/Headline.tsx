9c885e317203699055c33040ef73c8b7b3bbbc01
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-07-27 17:18:00 -0300

    Add animation to headline of introducing section

--- src/components/introducing/Headline.tsx
+++ src/components/introducing/Headline.tsx
@@ -6,9 +6,12 @@ export default function Headline() {
   const { elRef } = useSplitText()
 
   return (
-    <h2 ref={elRef} className='w-[39vw] -mt-28 text-kili-white text-7xl'>
-      Full Stack JavaScript Developer & UX/UI Designer with +5 years of work
-      experience.
+    <h2 ref={elRef} className='w-[39vw] -mt-28 text-kili-white text-7xl group'>
+      Full Stack JavaScript Developer & UX/UI Designer with{' '}
+      <span className='block transition-transform duration-700 ease-in-out group-hover:rotate-180'>
+        +
+      </span>
+      5 years of work experience.
     </h2>
   )
 }
