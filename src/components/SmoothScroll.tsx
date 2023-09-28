dc145dc78431e630025416b5a3f07045f54b3287
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-09-28 16:31:48 -0300

    Improve scroll locking

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
 import { useStore } from '@/lib/store/store'
 import type { ChildrenType } from '@/lib/interfaces/general'
 
 export default function SmoothScroll({ children }: { children: ChildrenType }) {
   const { navBarStatus, introRunning } = useStore()
+  const [stopScrolling, setStopScrolling] = useState(true)
   const lenis = useLenis()
 
   useEffect(() => {
@@ -16,12 +17,21 @@ export default function SmoothScroll({ children }: { children: ChildrenType }) {
 
     const headerIsVisible = window.scrollY < 100
 
-    if ((navBarStatus || introRunning) && headerIsVisible) {
+    const sessionStorage = window.sessionStorage.getItem('__LOCK_SCROLL__')
+
+    if (sessionStorage === null) {
+      window.sessionStorage.setItem('__LOCK_SCROLL__', true.toString())
+    } else {
+      window.sessionStorage.setItem('__LOCK_SCROLL__', false.toString())
+      setStopScrolling(false)
+    }
+
+    if ((navBarStatus || introRunning) && headerIsVisible && stopScrolling) {
       lenis.stop()
     } else {
       lenis.start()
     }
-  }, [lenis, navBarStatus, introRunning])
+  }, [lenis, navBarStatus, introRunning, stopScrolling])
 
   return <ReactLenis root>{children}</ReactLenis>
 }
