ceb6c2554cc0027a60ba2acaac855cdfffa4579a
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-10-12 16:31:49 -0300

    Remove focus-trap-react

--- src/components/contact-form/FormModal.tsx
+++ src/components/contact-form/FormModal.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useRef } from 'react'
-import FocusTrap from 'focus-trap-react'
 import { AnimatePresence, motion, useDragControls } from 'framer-motion'
 import ContactForm from '@/components/contact-form/Form'
 import { useStore } from '@/lib/store/store'
@@ -19,16 +18,14 @@ export default function FormModal() {
           ref={dragConstraintsRef}
           className='fixed inset-0 z-50 flex items-center justify-center pointer-events-none'
         >
-          <FocusTrap>
-            <motion.div
-              drag
-              dragConstraints={dragConstraintsRef}
-              dragControls={dragControls}
-              dragListener={false}
-            >
-              <ContactForm handleDrag={(event) => dragControls.start(event)} />
-            </motion.div>
-          </FocusTrap>
+          <motion.div
+            drag
+            dragConstraints={dragConstraintsRef}
+            dragControls={dragControls}
+            dragListener={false}
+          >
+            <ContactForm handleDrag={(event) => dragControls.start(event)} />
+          </motion.div>
         </motion.div>
       )}
     </AnimatePresence>
