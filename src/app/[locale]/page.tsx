49138fb2d6ba2af04b739f9f3fa2db20b6fbc9b3
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-10-16 17:00:20 -0300

    Test mobile

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,10 @@
 import { setStaticParamsLocale } from 'next-international/server'
 import Header from '@/components/sections/Header'
 import FeaturedProjects from '@/components/sections/FeaturedProjects'
-import Introducing from '@/components/sections/Introducing'
-import LessButBetter from '@/components/sections/LessButBetter'
-import MoreProjects from '@/components/sections/MoreProjects'
-import Knowledge from '@/components/sections/Knowledge'
+// import Introducing from '@/components/sections/Introducing'
+// import LessButBetter from '@/components/sections/LessButBetter'
+// import MoreProjects from '@/components/sections/MoreProjects'
+// import Knowledge from '@/components/sections/Knowledge'
 
 export default async function Home({
   params: { locale }
