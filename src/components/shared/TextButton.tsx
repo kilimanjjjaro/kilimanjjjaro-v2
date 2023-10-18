d6b19190308806d8bfa65a66e8a1b10cf861318a
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-10-18 15:37:25 -0300

    Improve underline in mobile

--- src/components/shared/TextButton.tsx
+++ src/components/shared/TextButton.tsx
@@ -23,7 +23,7 @@ export default function TextButton({
     <button
       className={clsx(
         underlined === true &&
-          'relative before:h-[1px] before:scale-x-100 before:absolute mb-2 before:-bottom-2 before:left-0 before:right-0 before:block before:origin-right xl:hover:before:scale-x-0 before:transition-transform before:ease-in xl:hover:before:ease-out before:duration-1000 before:delay-1000 xl:hover:before:delay-0 after:h-[1px] after:absolute after:-bottom-2 after:left-0 after:right-0 after:block after:origin-left after:scale-x-0 xl:hover:after:scale-x-100 after:transition-transform after:ease-in xl:hover:after:ease-out after:duration-1000 xl:hover:after:delay-1000',
+          'relative before:h-[1px] before:scale-x-100 before:absolute mb-1 xl:mb-2 before:-bottom-[6px] xl:before:-bottom-2 before:left-0 before:right-0 before:block before:origin-right xl:hover:before:scale-x-0 before:transition-transform before:ease-in xl:hover:before:ease-out before:duration-1000 before:delay-1000 xl:hover:before:delay-0 after:h-[1px] after:absolute after:-bottom-[6px] xl:after:-bottom-2 after:left-0 after:right-0 after:block after:origin-left after:scale-x-0 xl:hover:after:scale-x-100 after:transition-transform after:ease-in xl:hover:after:ease-out after:duration-1000 xl:hover:after:delay-1000',
         className ?? ''
       )}
       onClick={onClick}
