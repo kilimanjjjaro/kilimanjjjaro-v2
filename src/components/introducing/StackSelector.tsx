03130df704d4df580087ae008fefa6efcc5b8439
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-10-19 19:04:04 -0300

    Improve stack selector

--- src/components/introducing/StackSelector.tsx
+++ src/components/introducing/StackSelector.tsx
@@ -3,8 +3,10 @@
 import { useState } from 'react'
 import { motion } from 'framer-motion'
 import clsx from 'clsx'
+import { useLenis } from '@studio-freight/react-lenis'
 import { PlusIcon } from '@/components/icons/PlusIcon'
 import { useStore } from '@/lib/store/store'
+import useMediaQuery from '@/lib/hooks/useMediaQuery'
 import {
   STACKS_LI_VARIANTS,
   STACKS_UL_VARIANTS
@@ -21,8 +23,22 @@ export default function StackSelector() {
     setCursorStatus
   } = useStore()
   const [showSelector, setShowSelector] = useState(false)
+  const { isDesktop } = useMediaQuery()
+  const lenis = useLenis()
 
-  const handleClick = (stack: StackInterface) => {
+  const handleClickOnSelector = () => {
+    setShowSelector(!showSelector)
+
+    if (isDesktop) return
+
+    if (showSelector) {
+      lenis?.start()
+    } else {
+      lenis?.stop()
+    }
+  }
+
+  const handleClickOnSkills = (stack: StackInterface) => {
     setSelectedStack(stack)
     setShowSelector(false)
     setShouldMoveToStart(true)
@@ -31,7 +47,8 @@ export default function StackSelector() {
   return (
     <div className='flex gap-10 pl-6 text-4xl xl:pl-40 text-kili-white'>
       <button
-        onClick={() => setShowSelector(!showSelector)}
+        className='z-20 xl:z-0'
+        onClick={() => handleClickOnSelector()}
         onMouseEnter={() => setCursorStatus(CURSOR_STATUS.HOVER)}
         onMouseLeave={() => setCursorStatus(CURSOR_STATUS.DEFAULT)}
       >
@@ -46,7 +63,7 @@ export default function StackSelector() {
         </h2>
       </button>
       <motion.ul
-        className='hidden gap-10'
+        className='fixed left-0 right-0 z-10 flex-col items-center hidden gap-6 bottom-8 xl:gap-10 xl:flex-row xl:static'
         variants={STACKS_UL_VARIANTS}
         animate={showSelector ? 'open' : 'closed'}
         transition={{
@@ -55,6 +72,17 @@ export default function StackSelector() {
           staggerChildren: 0.1
         }}
       >
+        {!isDesktop && (
+          <motion.div
+            className='fixed bottom-0 left-0 w-full h-screen origin-bottom bg-gradient-to-t from-kili-black to-kili-black/0'
+            initial={{ scaleY: '0%' }}
+            animate={showSelector ? { scaleY: '100%' } : { scaleY: '0%' }}
+            transition={{
+              duration: 1,
+              ease: 'easeInOut'
+            }}
+          />
+        )}
         {stacks
           .filter((stack) => stack.id !== selectedStack.id)
           .map((stack) => (
@@ -63,7 +91,7 @@ export default function StackSelector() {
               className='overflow-hidden leading-none transition-colors duration-1000 ease-in-out xl:hover:text-kili-light-gray'
             >
               <motion.button
-                onClick={() => handleClick(stack)}
+                onClick={() => handleClickOnSkills(stack)}
                 onMouseEnter={() => setCursorStatus(CURSOR_STATUS.HOVER)}
                 onMouseLeave={() => setCursorStatus(CURSOR_STATUS.DEFAULT)}
                 variants={STACKS_LI_VARIANTS}
