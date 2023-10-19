f5c7e0785a0f6c04f5d03618c7a8d589c2f32197
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-10-18 21:56:27 -0300

    Fix underline

--- src/components/shared/Link.tsx
+++ src/components/shared/Link.tsx
@@ -3,8 +3,11 @@
 import NextLink from 'next/link'
 import clsx from 'clsx'
 import { useStore } from '@/lib/store/store'
-import { CURSOR_STATUS, UNDERLINED_STYLES } from '@/lib/constants/general'
+import { CURSOR_STATUS } from '@/lib/constants/general'
 import type { ChildrenType } from '@/lib/interfaces/general'
+
+const UNDERLINED_STYLES =
+  'relative before:h-[1px] before:scale-x-100 before:absolute mb-1 xl:mb-2 before:-bottom-[6px] xl:before:-bottom-2 before:left-0 before:right-0 before:block before:origin-right xl:hover:before:scale-x-0 before:transition-transform before:ease-in xl:hover:before:ease-out before:duration-1000 before:delay-1000 xl:hover:before:delay-0 after:h-[1px] after:absolute after:-bottom-[6px] xl:after:-bottom-2 after:left-0 after:right-0 after:block after:origin-left after:scale-x-0 xl:hover:after:scale-x-100 after:transition-transform after:ease-in xl:hover:after:ease-out after:duration-1000 xl:hover:after:delay-1000'
 
 interface Props {
   children: ChildrenType
