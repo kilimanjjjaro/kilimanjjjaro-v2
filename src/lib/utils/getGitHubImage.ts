9e5997c1221727389dd2301103cb7ede8bfe278e
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-11-06 17:28:40 -0300

    Fix github assets url

--- src/lib/utils/getGitHubImage.ts
+++ src/lib/utils/getGitHubImage.ts
@@ -1,7 +1,7 @@
 export const getGitHubImage = (projectSlug: string) => {
   const hash = crypto.randomUUID()
 
-  const image = `https://opengraph.githubassets.com/${hash}/monospace/${projectSlug}`
+  const image = `https://opengraph.githubassets.com/${hash}/kilimanjjjaro/${projectSlug}`
 
   return image
 }
