084e6c681c09f7f5408132530ebd093f0fd4ebc0
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-11-09 19:26:39 -0300

    fix: improve three js scene

--- src/components/scenes/hero-header/Scene.tsx
+++ src/components/scenes/hero-header/Scene.tsx
@@ -3,14 +3,19 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import PixalatedImage from '@/components/scenes/hero-header/PixalatedImage'
+import { useStore } from '@/lib/store/store'
+import { CURSOR_STATUS } from '@/lib/constants/general'
 
 const FRUSTUM_SIZE = 1
 const CANVAS_WIDTH = 360
 const CANVAS_HEIGHT = 480
 
 export default function HeroHeaderScene() {
+  const { setCursorStatus } = useStore()
+
   return (
     <Canvas
+      className='cursor-none'
       style={{ width: CANVAS_WIDTH, height: CANVAS_HEIGHT }}
       onCreated={({ gl }) => {
         gl.setClearColor(0x000000, 0)
@@ -25,6 +30,8 @@ export default function HeroHeaderScene() {
         far: 1000,
         zoom: 1000
       }}
+      onMouseEnter={() => setCursorStatus(CURSOR_STATUS.HOVER)}
+      onMouseLeave={() => setCursorStatus(CURSOR_STATUS.DEFAULT)}
     >
       <Suspense fallback={null}>
         <PixalatedImage src='/images/portrait.webp' />
