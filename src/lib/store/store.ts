ccca991d3d53d8095f4c1374dee6a3bc9aa41503
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-11-18 14:52:31 -0300

    feat: add shallow option in zustand

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,11 +1,15 @@
+import { shallow } from 'zustand/shallow'
 import { createWithEqualityFn } from 'zustand/traditional'
 import { createGeneralSlice } from '@/lib/store/slices/general'
 import { createNavbarSlice } from '@/lib/store/slices/navbar'
 import { createSkillsSlice } from '@/lib/store/slices/skills'
 import type { StoreInterface } from '@/lib/types/store'
 
-export const useStore = createWithEqualityFn<StoreInterface>((...a) => ({
-  ...createGeneralSlice(...a),
-  ...createNavbarSlice(...a),
-  ...createSkillsSlice(...a)
-}))
+export const useStore = createWithEqualityFn<StoreInterface>(
+  (...a) => ({
+    ...createGeneralSlice(...a),
+    ...createNavbarSlice(...a),
+    ...createSkillsSlice(...a)
+  }),
+  shallow
+)
