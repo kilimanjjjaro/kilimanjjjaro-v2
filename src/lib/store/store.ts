eb7064da9aadb0555166ee9f1e957351403015c0
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-11-18 14:28:00 -0300

    refactor: replace create store function

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,10 +1,10 @@
-import { create } from 'zustand'
+import { createWithEqualityFn } from 'zustand/traditional'
 import { createGeneralSlice } from '@/lib/store/slices/general'
 import { createNavbarSlice } from '@/lib/store/slices/navbar'
 import { createSkillsSlice } from '@/lib/store/slices/skills'
 import type { StoreInterface } from '@/lib/types/store'
 
-export const useStore = create<StoreInterface>((...a) => ({
+export const useStore = createWithEqualityFn<StoreInterface>((...a) => ({
   ...createGeneralSlice(...a),
   ...createNavbarSlice(...a),
   ...createSkillsSlice(...a)
