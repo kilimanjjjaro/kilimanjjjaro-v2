43a311ab96ead77bc4b65d548ccaed87296cb3a9
Kilimanjjjaro <hola@kilimanjjjaro.com>
2024-01-29 19:15:59 -0300

    feat: update prettier config

---
+++ .prettierrc.mjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  endOfLine: 'lf'
+}
