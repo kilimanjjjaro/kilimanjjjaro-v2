7447c17c6b573e762ee380ad06bcb0f98040dbb8
Kilimanjjjaro <hola@kilimanjjjaro.com>
2024-01-29 19:08:20 -0300

    feat: update header

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,10 +1,8 @@
-import Headline from '@/components/header/Headline'
 import Aside from '@/components/header/Aside'
 
 export default function Header() {
   return (
     <header className='relative flex items-center justify-center px-6 pt-48 pb-44 xl:p-0 min-h-screen xl:w-full'>
-      <Headline />
       <Aside />
     </header>
   )
