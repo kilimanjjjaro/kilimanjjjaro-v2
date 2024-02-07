17d1b8fe62e386444723ffc67c61e0aa4f9fa9df
Kilimanjjjaro <hola@kilimanjjjaro.com>
2024-02-06 21:42:22 -0300

    feat: add fade text

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,41 +1,25 @@
 'use client'
 
 import { useRef } from 'react'
-import { motion, useScroll, useTransform } from 'framer-motion'
-import FeaturedProject from '@components/projects/FeaturedProject'
-import { useCurrentLocale } from '@lib/i18n/client'
-import { LOCALES } from '@lib/constants/general'
-import { FEATURED_PROJECTS } from '@lib/constants/projects'
+import FadeTextOnScroll from '@components/shared/FadeTextOnScroll'
 
 export default function Projects() {
-  const sectionEl = useRef<HTMLElement>(null)
-  const currentLocale = useCurrentLocale()
-  const { scrollYProgress } = useScroll({
-    target: sectionEl,
-    offset: ['start end', 'end end']
-  })
-
-  const y = useTransform(scrollYProgress, [0, 1], [0, -200])
-
-  const featuredProjects =
-    currentLocale === LOCALES.en ? FEATURED_PROJECTS.en : FEATURED_PROJECTS.es
+  const containerEl = useRef<HTMLDivElement>(null)
 
   return (
-    <motion.section id='projects' ref={sectionEl} className='xl:px-44 xl:py-44'>
-      <h2 className='mb-28 flex flex-col text-10xl font-semibold uppercase text-monospace-white'>
-        <span className='translate-x-32'>Some projects </span>
-        <span>to highlight</span>
-      </h2>
-      <p className='mb-32 ml-60 w-2/3 text-balance text-5xl leading-semi-tight text-monospace-light-gray'>
-        <span className='block'>Since 2017_</span> designing and developing for
-        brands, agencies and independent studios. Below you can see some of
-        them.
-      </p>
-      <section className='grid grid-cols-2 xl:gap-32'>
-        {featuredProjects.map((project, index) => (
-          <FeaturedProject key={project.id} project={project} index={index} />
-        ))}
-      </section>
-    </motion.section>
+    <section id='projects'>
+      <div
+        ref={containerEl}
+        className='h-250-vh relative max-h-[2500px] xl:px-44'
+      >
+        <div className='h-100-vh sticky top-0 flex w-full flex-col items-start justify-center'>
+          <FadeTextOnScroll
+            className='text-6xl font-medium text-monospace-white'
+            target={containerEl}
+            text='Since 2017, designing and developing for brands, agencies and independent studios. Below you can see some of them.'
+          />
+        </div>
+      </div>
+    </section>
   )
 }
---
+++ src/components/shared/FadeTextOnScroll.tsx
@@ -0,0 +1,65 @@
+import { useMemo } from 'react'
+import {
+  type MotionValue,
+  motion,
+  useScroll,
+  useTransform
+} from 'framer-motion'
+import type { ChildrenType } from '@lib/types/general'
+
+interface WordProps {
+  children: ChildrenType
+  range: number[]
+  scrollProgress: MotionValue<number>
+}
+
+interface FadeTextOnScrollProps {
+  text: string
+  target: React.RefObject<HTMLDivElement>
+  className: string
+}
+
+const Word = ({ children, range, scrollProgress }: WordProps) => {
+  const opacity = useTransform(scrollProgress, range, [0, 1])
+
+  return (
+    <motion.span className='inline-block' style={{ opacity }}>
+      {children}
+    </motion.span>
+  )
+}
+
+export default function FadeTextOnScroll({
+  text,
+  target,
+  className
+}: FadeTextOnScrollProps) {
+  const { scrollYProgress } = useScroll({
+    target,
+    offset: ['start start', 'end end'],
+    layoutEffect: false
+  })
+
+  const splitedText = useMemo(() => {
+    return text.split(' ')
+  }, [text])
+
+  return (
+    <p className={`flex flex-wrap gap-x-[0.4ch] ${className}`}>
+      {splitedText.map((word, index) => {
+        const start = index / splitedText.length
+        const end = start + 1 / splitedText.length
+
+        return (
+          <Word
+            key={index}
+            scrollProgress={scrollYProgress}
+            range={[start, end]}
+          >
+            {word}
+          </Word>
+        )
+      })}
+    </p>
+  )
+}
